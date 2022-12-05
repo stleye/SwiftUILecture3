3dd19c0a302be1481788a715e27f91e06776281f
stleye <stleye@gmail.com>
2022-12-05 14:35:40 -0300

    Added SwiftUILecture3 Code

---
+++ SwiftUILecture3/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  SwiftUILecture3
+//
+//  Created by Sebastian Tleye on 05/12/2022.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    private var cards: [Card]
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+
+    func choose(_ card: Card) {
+        
+    }
+
+    struct Card {
+        var isFaceUp = false
+        var isMatched = false
+        var content: CardContent
+    }
+    
+}
---
+++ SwiftUILecture3/SwiftUILecture3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILecture3App.swift
+//  SwiftUILecture3
+//
+//  Created by Sebastian Tleye on 04/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILecture3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
